4e2075f0bf6554e7f2d4b222c98ad5147828c47f
arjanjohan <arjanvanhoogdalem@gmail.com>
2024-07-04 21:52:01 +0200

    delete unused files

--- packages/nextjs/contracts/externalMoveContracts.ts
+++
@@ -1,8 +0,0 @@
-
-import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
-
-const externalContracts = {
-  "devnet": []
-} as const ;
-
-export default externalContracts satisfies GenericContractsDeclaration;
