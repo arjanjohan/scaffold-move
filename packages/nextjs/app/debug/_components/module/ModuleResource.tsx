dab5b8554fe36e0dc6096307fef7d34321223d36
arjanjohan <arjanvanhoogdalem@gmail.com>
2024-09-14 16:46:53 +0200

    code styling resources debug tab

--- packages/nextjs/app/debug/_components/module/ModuleResource.tsx
+++ packages/nextjs/app/debug/_components/module/ModuleResource.tsx
@@ -1,4 +1,5 @@
 import dynamic from "next/dynamic";
+import { useTheme } from "next-themes";
 import { Types } from "aptos";
 
 const DynamicReactJson = dynamic(() => import("react-json-view"), { ssr: false });
@@ -11,17 +12,22 @@ interface ModuleResourceProps {
 const GROUP_ARRAYS_AFTER_LENGTH = 100;
 const COLLAPSE_STRINGS_AFTER_LENGTH = 80;
 
-export const ModuleResource = ({ resource, collapsedByDefault }: ModuleResourceProps) => (
-  <div>
-    <div>{resource.type}</div>
-    <DynamicReactJson
-      src={resource.data}
-      collapseStringsAfterLength={COLLAPSE_STRINGS_AFTER_LENGTH}
-      displayObjectSize={false}
-      displayDataTypes={false}
-      quotesOnKeys={false}
-      groupArraysAfterLength={GROUP_ARRAYS_AFTER_LENGTH}
-      collapsed={collapsedByDefault}
-    />
-  </div>
-);
+export const ModuleResource = ({ resource, collapsedByDefault }: ModuleResourceProps) => {
+  const { resolvedTheme } = useTheme();
+  const isDarkMode = resolvedTheme === "dark";
+  return (
+    <div>
+      <div>{resource.type}</div>
+      <DynamicReactJson
+        src={resource.data}
+        collapseStringsAfterLength={COLLAPSE_STRINGS_AFTER_LENGTH}
+        displayObjectSize={false}
+        displayDataTypes={false}
+        quotesOnKeys={false}
+        groupArraysAfterLength={GROUP_ARRAYS_AFTER_LENGTH}
+        collapsed={collapsedByDefault}
+        theme={isDarkMode ? "twilight" : undefined}
+      />
+    </div>
+  );
+};
