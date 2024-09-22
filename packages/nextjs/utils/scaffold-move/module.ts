4a1304262f07840c16650ded3882ca0337ef5916
arjanjohan <arjanvanhoogdalem@gmail.com>
2024-09-22 07:24:31 +0200

    minimal modules

--- packages/nextjs/utils/scaffold-move/module.ts
+++ packages/nextjs/utils/scaffold-move/module.ts
@@ -5,81 +5,11 @@ import deployedModulesData from "~~/modules/deployedModules";
 import externalModulesData from "~~/modules/externalModules";
 import scaffoldConfig from "~~/scaffold.config";
 
-type AddExternalFlag<T> = {
-  [ChainId in keyof T]: {
-    [ModuleName in keyof T[ChainId]]: T[ChainId][ModuleName] & { external?: true };
-  };
-} & {
-  // Added this index signature to allow for flexibility with key types // TODO: Figure out how to properly handle this
-  [key: string]: any;
-};
-
-const deepMergeModules = <L extends Record<PropertyKey, any>, E extends Record<PropertyKey, any>>(
-  local: L,
-  external: E,
-) => {
-  const result: Record<PropertyKey, any> = {};
-  const allKeys = Array.from(new Set([...Object.keys(external), ...Object.keys(local)]));
-  for (const key of allKeys) {
-    if (!external[key]) {
-      result[key] = local[key];
-      continue;
-    }
-    const amendedExternal = Object.fromEntries(
-      Object.entries(external[key] as Record<string, Record<string, unknown>>).map(([moduleName, declaration]) => [
-        moduleName,
-        { ...declaration, external: true },
-      ]),
-    );
-    result[key] = { ...local[key], ...amendedExternal };
-  }
-  return result as MergeDeepRecord<AddExternalFlag<L>, AddExternalFlag<E>, { arrayMergeMode: "replace" }>;
-};
-
-const modulesData = deepMergeModules(deployedModulesData, externalModulesData);
+const modulesData = deployedModulesData;
 
 export type GenericModule = {
   bytecode: string;
-  abi: GenericModuleAbi;
-};
-
-export type GenericModuleAbi = {
-  address: Types.Address;
-  name: string;
-  friends: readonly string[];
-  exposed_functions: readonly MoveFunction[];
-  structs: readonly MoveStruct[];
-};
-
-type MoveFunction = {
-  name: string;
-  visibility: string;
-  is_entry: boolean;
-  is_view: boolean;
-  generic_type_params: readonly MoveFunctionGenericTypeParam[];
-  params: readonly string[];
-  return: readonly string[];
-};
-
-type MoveFunctionGenericTypeParam = {
-  constraints: readonly string[];
-};
-
-type MoveStruct = {
-  name: string;
-  is_native: boolean;
-  abilities: readonly string[];
-  generic_type_params: readonly MoveStructGenericTypeParam[];
-  fields: readonly MoveStructField[];
-};
-
-type MoveStructGenericTypeParam = {
-  constraints: readonly string[];
-};
-
-type MoveStructField = {
-  name: string;
-  type: string;
+  // abi: GenericModuleAbi;
 };
 
 export type GenericModulesDeclaration = {
@@ -90,7 +20,7 @@ export type GenericModulesDeclaration = {
 
 export const modules = deployedModulesData as GenericModulesDeclaration | null;
 
-type ConfiguredChainId = (typeof scaffoldConfig)["targetNetworks"][0]["id"];
+type ConfiguredChainId = 0;
 
 type IsModuleDeclarationMissing<TYes, TNo> = typeof modulesData extends { [key in ConfiguredChainId]: any }
   ? TNo
