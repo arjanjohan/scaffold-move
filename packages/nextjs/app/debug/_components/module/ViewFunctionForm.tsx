df68f77090c86b97988f240f73526eef4cd41a5c
arjanjohan <arjanvanhoogdalem@gmail.com>
2024-10-14 16:43:22 +0200

    add texts to bio page

--- packages/nextjs/app/debug/_components/module/ViewFunctionForm.tsx
+++ packages/nextjs/app/debug/_components/module/ViewFunctionForm.tsx
@@ -3,7 +3,7 @@ import { Types } from "aptos";
 import { displayTxResult } from "~~/app/debug/_components/module";
 import { useAptosClient } from "~~/hooks/scaffold-move";
 import { useTargetNetwork } from "~~/hooks/scaffold-move/useTargetNetwork";
-import { view } from "~~/utils/scaffold-move/view";
+import { view } from "~~/hooks/scaffold-move/useView";
 
 const zeroInputs = false;
 
