b99195ef9f206e89c1e0809d1fceb253a56da7c1
arjanjohan <arjanvanhoogdalem@gmail.com>
2024-12-23 08:23:41 +0100

    add api key to aptosclient

--- packages/nextjs/hooks/scaffold-move/useAptosClient.ts
+++ packages/nextjs/hooks/scaffold-move/useAptosClient.ts
@@ -1,6 +1,8 @@
 import { defaultChains } from "../../utils/scaffold-move/chains";
 import { Aptos, AptosConfig, Network } from "@aptos-labs/ts-sdk";
 
+const aptosApiKey = process.env.NEXT_PUBLIC_APTOS_API_KEY;
+
 export function useAptosClient(chainId: number) {
   const chain = Object.values(defaultChains).find(chain => chain.id === chainId);
   if (!chain) {
@@ -14,6 +16,9 @@ export function useAptosClient(chainId: number) {
       indexer: chain.indexer,
       faucet: chain.faucet,
     }),
+    ...(aptosApiKey && {
+      apiKey: aptosApiKey
+    })
   });
   return new Aptos(aptosConfig);
 }
